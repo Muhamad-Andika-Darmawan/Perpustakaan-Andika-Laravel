bb17471b06ef392ebc4a650841e24af973b93fb4
Muhamad Andika Darmawan <226209850+Muhamad-Andika-Darmawan@users.noreply.github.com>
2026-06-02 07:51:58 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
+        html {
+            scroll-behavior: smooth;
+        }
         body { font-family: 'Inter', sans-serif; }
     </style>
 </head>
